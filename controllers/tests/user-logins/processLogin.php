c5563e3b6e3a759ea79db2af790a59436059b8a5
Andre Braithwaite <andrebwt@gmail.com>
2017-07-24 12:52:39 +0100

    Creating login session

--- controllers/tests/user-logins/processLogin.php
+++ controllers/tests/user-logins/processLogin.php
@@ -1,49 +1,77 @@
-<?php
-echo 'Processing login...' . '<br><br>';
+<?php header('user-page.php');
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+    echo 'Processing login...' . '<br><br>';
 
-//$userExists = false;
-$passwordCorrect = false;
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $do_login = false;
 
-echo 'username entered: ' . $username . '<br>';
-echo 'password entered: ' . $password . '<br><br>';
+    echo 'username entered: ' . $username . '<br>';
+    echo 'password entered: ' . $password . '<br><br>';
 
-function userFound($xml, $username) {
-    $userExists = false;
-    foreach ($xml->children() as $user) {
-        if ($username == $user->username) {
-            $userExists = true;
+    function userFound($xml, $username) {
+        $userExists = false;
+        foreach ($xml->children() as $user) {
+            if ($username == $user->username) {
+                $userExists = true;
+            }
+        }
+
+        if ($userExists) {
+            echo 'user ' . $username . ' was found.' . '<br>';
+            return true;
+        } else {
+            echo 'user ' . $username . ' not found!' . '<br>';
+            return false;
         }
     }
-    return $userExists;
-}
 
-
-
-if(file_exists('../../../models/tests/user-logins/test-users.xml')){
-
-    echo 'user database exists' . '<br>';
-
-    $xml = new SimpleXMLElement('../../../models/tests/user-logins/test-users.xml', 0, true);
-
-    // Look for user
-    echo userFound($xml, $username) ? 'user ' . $username . ' was found.' . '<br>' :
-        'user ' . $username . ' not found!' . '<br>';
-
-/*
-    if($userExists == true) {
-
-        // Refactor as function. DRY
+    function passCorrect($xml, $username, $password) {
         foreach ($xml->children() as $user) {
             if ($username == $user->username && $password == $user->password) {
                 echo '<br>' . 'password is good.';
-            } else if($username == $user->username && $password != $user->password) {
+                return true;
+            } else if ($username == $user->username && $password != $user->password) {
                 echo '<br>' . 'password is bad!';
+                return false;
             }
         }
     }
-*/
 
+    if(file_exists('../../../models/tests/user-logins/test-users.xml')){
+
+        echo 'user database exists' . '<br>';
+
+        $xml = new SimpleXMLElement('../../../models/tests/user-logins/test-users.xml', 0, true);
+
+        // If  user is found check the password is correct
+        if (userFound($xml, $username)){
+            $do_login = passCorrect($xml, $username, $password);
+        }
+
+        // If the username and password are correct, start a new session
+        if($do_login) {
+            echo '<br>' . 'Starting session...';
+
+            session_start();
+            $_SESSION['username'] = $username;
+            echo("<script>window.location = 'user-page.php';</script>");
+            die;
+        }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Test User Logins</title>
+        <link rel="stylesheet" type="text/css" href="../../styles.css">
+    </head>
+
+    <body>
+        <br><br>
+        <a href="../../../views/tests/user-logins/user-logins.html">Try Again</a>
+    </body>
+</html>
