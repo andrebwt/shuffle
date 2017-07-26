393cb9347b2933ca968fd529a1ab720dc4597ad4
Andre Braithwaite <andrebwt@gmail.com>
2017-07-26 16:45:36 +0100

    Refactored login code.

--- controllers/tests/user-logins/processLogin.php
+++ controllers/tests/user-logins/processLogin.php
@@ -5,6 +5,7 @@
     $do_login = false;
 
     function userFound($xml, $username) {
+
         $userExists = false;
         foreach ($xml->children() as $user) {
             if ($username == $user->username) {
@@ -13,21 +14,19 @@
         }
 
         if ($userExists) {
-            echo 'user ' . $username . ' was found.' . '<br>';
             return true;
         } else {
-            echo 'user ' . $username . ' not found!' . '<br>';
             return false;
         }
     }
 
     function passCorrect($xml, $username, $password) {
+
         foreach ($xml->children() as $user) {
             if ($username == $user->username && $password == $user->password) {
-                echo '<br>' . 'password is good.';
                 return true;
             } else if ($username == $user->username && $password != $user->password) {
-                echo '<br>' . 'password is bad!';
+                echo '<input type="button" value = "WRONG PASSWORD!" style="background:darkred;color: #FFFFFF">';
                 return false;
             }
         }
@@ -39,6 +38,7 @@
 <html lang="en">
 
     <head>
+
         <meta charset="UTF-8">
         <title>Test User Logins</title>
         <link rel="stylesheet" type="text/css" href="../../../views/styles.css">
@@ -50,21 +50,23 @@
         <div class="status-info">
 
             <input type="button" value = 'PROCESSING LOGIN...' style="background:#4CAF50;color: #FFFFFF">
-            <input type = "button" value="<?php echo 'username entered: ' . $username;?>">
 
+            <input type = "button" value="<?php echo 'username entered: ' . $username;?>">
             <input type = "button" value="<?php echo 'password entered: ' . $password;?>" />
 
             <?php
             if(file_exists('../../../models/tests/user-logins/test-users.xml')){
 
-                //echo 'user database exists' . '<br>';
                 echo '<input type="button" value = "USER DATABASE EXISTS" style="background:#4CAF50;color: #FFFFFF">';
 
                 $xml = new SimpleXMLElement('../../../models/tests/user-logins/test-users.xml', 0, true);
 
                 // If  user is found check the password is correct
                 if (userFound($xml, $username)){
+                    echo '<input type="button" value = "USER FOUND" style="background:#4CAF50;color: #FFFFFF">';
                     $do_login = passCorrect($xml, $username, $password);
+                } else {
+                    echo '<input type="button" value = "USER NOT FOUND!" style="background:darkred;color: #FFFFFF">';
                 }
 
                 // If the username and password are correct, start a new session
@@ -76,20 +78,15 @@
                     echo("<script>window.location = 'user-page.php';</script>");
                     die;
                 }
+            } else {
+                echo '<input type="button" value = "USER DATABASE NOT FOUND!" style="background:darkred;color: #FFFFFF">';
             }
             ?>
-
-
-
         </div>
 
         <div class="status-info">
             <button type="button" onclick="window.location='../../../views/tests/user-logins/user-logins.html';">
                 try again</button>
         </div>
-
-
-
-    </body>
     </body>
 </html>
